d5b2e9937dbbe52ee1409262e30d7fb38f819fdf
Danny Tuppeny <danny@tuppeny.com>
2021-04-29 11:41:23 +0100

    More Dart tests into dart folder

--- test/dart/no_debug_test.dart
+++ test/dart/no_debug_test.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:dap/src/adapters/dart.dart';
 import 'package:dap/src/debug_adapter_protocol_generated.dart';
 import 'package:test/test.dart';
+import 'package:path/path.dart' as path;
 
-import 'server.dart';
-import 'test_utils.dart';
+import '../server.dart';
+import '../test_utils.dart';
 
 void main() {
   test('Server runs a simple script in noDebug mode', () async {
@@ -26,7 +30,7 @@ void main() {
       client.event('terminated'),
       client.sendRequest(
         'launch',
-        LaunchRequestArguments(
+        DartLaunchRequestArguments(
           noDebug: true,
           program: 'hello_world.dart',
           cwd: await testApplicationsDirectory,
