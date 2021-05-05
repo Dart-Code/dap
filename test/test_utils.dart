4f47dd57e8b8e5de0accb1f37bfafe3ed3e90619
Danny Tuppeny <danny@tuppeny.com>
2021-05-05 17:05:05 +0100

    Fix path on Windows

--- test/test_utils.dart
+++ test/test_utils.dart
@@ -19,7 +19,8 @@ final Future<String> testApplicationsDirectory = (() async => path.join(
     path.dirname(path.dirname(
         (await Isolate.resolvePackageUri(Uri.parse('package:dap/dap.dart')))!
             .toFilePath())),
-    'test/test_applications'))();
+    'test',
+    'test_applications'))();
 
 int lineWith(File file, String searchText) =>
     file.readAsLinesSync().indexWhere((line) => line.contains(searchText));
